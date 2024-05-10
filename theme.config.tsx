b8ed247b6b6e438fce6c43e5481aa0dab4a2de51
Sarim <hello@sarim.us>
2024-05-09 18:19:37 -0600

    First commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,61 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
+import { Charis_SIL } from "next/font/google"
+import { useRouter } from 'next/router'
+
+const font = Charis_SIL({
+  weight: "400",
+  subsets: ["latin"],
+})
+
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span style={{ fontSize: 30 }} className={`${font.className}`}>WikiSubmission</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/wikisubmission/website',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/wikisubmission/website',
   footer: {
-    text: 'Nextra Docs Template',
+    text: (
+      <span className={`${font.className}`}>
+        {new Date().getFullYear()} ©{' '}
+        <a href="https://wikisubmission.org" target="_blank">
+          <strong>WikiSubmission</strong>
+        </a>
+      </span>
+    )
   },
+  primaryHue: 360,
+  primarySaturation: 50,
+  search: {
+    placeholder: "Search WikiSubmission..."
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – WikiSubmission'
+      }
+    }
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://wikisubmission.org' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'WikiSubmission'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'The Submission Knowledge Sharing Platform'}
+        />
+      </>
+    )
+  }
 }
 
 export default config
