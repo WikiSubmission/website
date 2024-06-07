f6c54e55444c73a04cf393b4008769066fada59b
Sarim <sarim.xs@gmail.com>
2024-06-06 18:35:25 -0600

    Early commit

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { locales } from 'nextra/locales';
-import { NextResponse } from 'next/server';
-import { NextRequest } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 
 export const middleware = request => {
 
@@ -10,6 +9,15 @@ export const middleware = request => {
 
     if (request.nextUrl.pathname.startsWith('/library')) { 
         return NextResponse.redirect(new URL(`https://docs.wikisubmission.org${request.nextUrl.pathname}`))
+    }
+
+    if (request.nextUrl.pathname.startsWith('/appendix/')) {
+        const appendixNumber = `${request?.nextUrl?.pathname?.split('/')[1] || '1'}`;
+        if (parseInt(appendixNumber) >= 1 && parseInt(appendixNumber) <= 38) { 
+            return NextResponse.redirect(new URL(`https://docs.wikisubmission.org/library/books/quran-the-final-testament-appendix-${appendixNumber}`, ))
+        } else { 
+            return NextResponse.redirect(new URL(`https://docs.wikisubmission.org/library/books/quran-the-final-testament-appendices`))
+        }
     }
 
     if (request.nextUrl.pathname.startsWith('/appendix-')) {
