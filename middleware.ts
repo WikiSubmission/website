d98be7259b59bd88d6969c20305f93fe50e3837f
Sarim <sarim.xs@gmail.com>
2024-07-28 11:37:45 -0600

    /appendix/0 --> introduction

--- middleware.ts
+++ middleware.ts
@@ -3,37 +3,39 @@ import { NextResponse, NextRequest } from 'next/server';
 
 export const middleware = request => {
 
-    if (request.nextUrl.pathname === '/authorize-discord') { 
+    if (request.nextUrl.pathname === '/authorize-discord') {
         return NextResponse.redirect(new URL(`https://discord.com/oauth2/authorize?client_id=978658099474890793&permissions=274877926400&scope=bot%20applications.commands`))
     }
 
-    if (request.nextUrl.pathname.startsWith('/library')) { 
+    if (request.nextUrl.pathname.startsWith('/library')) {
         return NextResponse.redirect(new URL(`https://docs.wikisubmission.org${request.nextUrl.pathname}`))
     }
 
     if (request.nextUrl.pathname.startsWith('/appendix/')) {
-        console.log(request.nextUrl.pathname);
         const appendixNumber = `${request?.nextUrl?.pathname?.split('/')[2] || '1'}`;
-        if (parseInt(appendixNumber) >= 1 && parseInt(appendixNumber) <= 38) { 
-            return NextResponse.redirect(new URL(`https://docs.wikisubmission.org/library/books/quran-the-final-testament-appendix-${appendixNumber}`, ))
-        } else { 
+        if (parseInt(appendixNumber) === 0) {
+            return NextResponse.redirect(new URL(`https://docs.wikisubmission.org/library/books/quran-the-final-testament-introduction`,))
+        }
+        else if (parseInt(appendixNumber) >= 1 && parseInt(appendixNumber) <= 38) {
+            return NextResponse.redirect(new URL(`https://docs.wikisubmission.org/library/books/quran-the-final-testament-appendix-${appendixNumber}`,))
+        } else {
             return NextResponse.redirect(new URL(`https://docs.wikisubmission.org/library/books/quran-the-final-testament-appendices`))
         }
     }
 
     if (request.nextUrl.pathname.startsWith('/appendix-')) {
         const appendixNumber = `${request?.nextUrl?.pathname?.split('-')[1] || '1'}`;
-        if (parseInt(appendixNumber) === 0) { 
-            return NextResponse.redirect(new URL(`https://docs.wikisubmission.org/library/books/quran-the-final-testament-introduction`, ))
+        if (parseInt(appendixNumber) === 0) {
+            return NextResponse.redirect(new URL(`https://docs.wikisubmission.org/library/books/quran-the-final-testament-introduction`,))
         }
-        else if (parseInt(appendixNumber) >= 1 && parseInt(appendixNumber) <= 38) { 
-            return NextResponse.redirect(new URL(`https://docs.wikisubmission.org/library/books/quran-the-final-testament-appendix-${appendixNumber}`, ))
-        } else { 
+        else if (parseInt(appendixNumber) >= 1 && parseInt(appendixNumber) <= 38) {
+            return NextResponse.redirect(new URL(`https://docs.wikisubmission.org/library/books/quran-the-final-testament-appendix-${appendixNumber}`,))
+        } else {
             return NextResponse.redirect(new URL(`https://docs.wikisubmission.org/library/books/quran-the-final-testament-appendices`))
         }
     }
 
-    if (request.nextUrl.pathname === '/appendices') { 
+    if (request.nextUrl.pathname === '/appendices') {
         return NextResponse.redirect(new URL(`https://docs.wikisubmission.org/library/books/quran-the-final-testament-appendices`))
     }
 
